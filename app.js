5e62298ad49cb6cbe6838e51891c137c8334aacb
Siyasanga <siyasanga@live.co.za>
2017-09-14 17:53:54 +0200

    Configure body-parser

--- app.js
+++ app.js
@@ -6,14 +6,28 @@ app.engine("hbs",exhbs({ defaultLayout:"main",
                          extname:"hbs"}));
 app.set("view engine","hbs");
 app.use(express.static("public"));
+app.use(body.urlencoded({
+  extended:false
+}));
+app.use(body.json());
 app.get("/",function(req, res) {
   res.render("home");
 });
-app.get("/user",function(req, res) {
+app.get("/:username",function(req, res) {
   res.render("empLogin");
 });
-app.post("/",function(req, res) {
-  res.render("empLogin",{submitted:true});
+app.post("/:username",function(req, res) {
+  console.log(req.params.username+'\n'+req.body.days);
+  res.redirect('/' + req.params.username);
+});
+
+app.post("/login",function(req, res) {
+  console.log(req.body);
+  if (req.body.username == "Admin") {
+    res.render("adminLogin");
+  }else {
+    res.redirect("/"+req.body.username);
+  }
 });
 app.post("/registration",function(req, res) {
   console.log(req.body);
