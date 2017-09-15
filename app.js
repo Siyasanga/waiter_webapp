421a400ec305ac4f66395cc8807273c6149daf27
Siyasanga <siyasanga@live.co.za>
2017-09-15 18:08:17 +0200

    Add validation for ragistration form

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const exhbs = require('express-handlebars');
 const body = require('body-parser');
 const express = require('express');
+const database = require('./database');
+const flash = require('express-flash');
+const session = require('express-session');
 var app = express();
 app.engine("hbs",exhbs({ defaultLayout:"main",
                          extname:"hbs"}));
+app.use(session({secret:"keyboard cat", cookie: { maxAge: 60000}}));
+app.use(flash());
 app.set("view engine","hbs");
 app.use(express.static("public"));
 app.use(body.urlencoded({
@@ -14,26 +19,52 @@ app.get("/",function(req, res) {
   res.render("home");
 });
 app.get("/:username",function(req, res) {
+  console.log(req.params.username);
   res.render("empLogin");
+});
+app.get("/home/Admin",function(req, res) {
+  res.render("adminLogin");
 });
 app.post("/:username",function(req, res) {
   console.log(req.params.username+'\n'+req.body.days);
   res.redirect('/' + req.params.username);
 });
-
-app.post("/login",function(req, res) {
-  console.log(req.body);
+app.post("/home/login",function(req, res) {
   if (req.body.username == "Admin") {
-    res.render("adminLogin");
+    console.log(req.body.username);
+    res.redirect(req.body.username);
   }else {
     res.redirect("/"+req.body.username);
   }
 });
-app.post("/registration",function(req, res) {
-  console.log(req.body);
-  res.render("adminLogin");
+app.post("/home/registration",function(req, res) {
+  if(req.body.pass !== req.body.confirm){
+    req.flash("mismatch","Your password and confirmation do not match.");
+    res.redirect("/");
+  }else {
+    database.employee.findOne({username:req.body.username},function(err,doc) {
+      if(err) {
+        console.log(err);
+      }else if(doc){
+        console.log("Inside findOne: \n"+doc);
+        req.flash("duplicateUser","Username already exists.");
+        res.redirect("/");
+      }
+      else {
+        var newUser = new database.employee({name:req.body.firstname, username:req.body.username, password:req.body.pass});
+        newUser.save(function(err, doc) {
+          if(err)
+            console.log(err);
+          else {
+            console.log("successfuly registered:\n"+doc);
+            req.flash("regSuccess","New account successfuly registered!");
+            res.redirect("/");
+          }
+        });
+      }
+    });
+  }
 });
-
 var port = process.env.PORT || 3000;
 var host = process.env.HOST || "http://localhost";
 app.listen(port, function() {
