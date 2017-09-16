ab6a4db931266310c6ea3be456b5f07b73e3befc
Siyasanga <siyasanga@live.co.za>
2017-09-16 16:01:26 +0200

    Work on updating availability

--- app.js
+++ app.js
@@ -30,15 +30,35 @@ app.get("/home/Admin",function(req, res) {
 // Updating availability of the logged in employee
 app.post("/:username",function(req, res) {
   console.log(req.params.username+'\n'+req.body.days);
-  res.redirect('/' + req.params.username);
+  var obj = JSON.parse(setDays(req.body.days));
+  console.log(obj[0]);
+  // database.weeklyShift.update({name:req.params.username},{$set:{eval("monday:true")}},function(err,result) {
+  //   if(err)
+  //     console.log("Could not update weeklyShift:\n"+err);
+  //     res.redirect('/'+req.params.username);
+  //   else {
+  //     console.log(result);
+  //     res.redirect('/'+req.params.username);
+  //   }
+  // });
 });
 // Managing paths for logins
 app.post("/home/login",function(req, res) {
+  console.log(req.body);
   if (req.body.username == "Admin") {
     res.redirect(req.body.username);
-  }else {
-    res.redirect("/"+req.body.username);
   }
+  database.employee.findOne({username:req.body.username, password:req.body.password},function(err,doc) {
+    if(err)
+      console.log("Error finding these credetials on the database:\n"+err);
+    else if(doc) {
+      console.log("These credetials match:\n"+doc);
+      res.redirect("/"+doc.name);
+    }else {
+      req.flash("failedLogin","Incorrect username and password.");
+      res.redirect("/");
+    }
+  }) // end of findOne
 });
 // Processing the registration of a new employee
 app.post("/home/registration",function(req, res) {
@@ -50,7 +70,7 @@ app.post("/home/registration",function(req, res) {
     // Checking if this is not a duplicate user
     database.employee.findOne({username:req.body.username},function(err,doc) {
       if(err) {
-        console.log(err);
+        console.log("Era yam:\n"+err);
       }else if(doc){
         req.flash("duplicateUser","Username already exists.");
         res.redirect("/");
@@ -62,14 +82,20 @@ app.post("/home/registration",function(req, res) {
           if(err)
             console.log(err);
           else {
-            // Creating an entry for this new user in the weeklyShift collection
-            var workerShifts = new database.weeklyShift(newUser.name);
-            console.log(workerShifts);
             console.log("successfuly registered:\n"+doc);
             req.flash("regSuccess","New account successfuly registered!");
-            res.redirect("/");
-          }
-        });
+            // Creating an entry for this new user in the weeklyShift collection
+            var workerShifts = new database.weeklyShift({name:newUser.name});
+            workerShifts.save(function(err, doc) {
+              if(err)
+                console.log("Error creating entry for new employee in weeklyShift:\n"+err);
+              else {
+                console.log("Entry created for new employee in the weeklyShift collection:\n"+doc);
+                res.redirect("/");
+              }
+            }); // end of inner save
+          } // end of outer else
+        }); // end of save
       }
     });
   }
@@ -79,3 +105,10 @@ var host = process.env.HOST || "http://localhost";
 app.listen(port, function() {
   console.log("Server running at "+host+":"+port+"/");
 });
+function setDays(days) {
+  var result = "{"+days[0]+":"+true;
+  for(var i=1; i<days.length; i++){
+    result += ", "+days[i]+":"+true;
+  }
+  return result+"}";
+}
