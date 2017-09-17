971f4a1001c2f8502546b0b5e698d3679fb6646f
Siyasanga <siyasanga@live.co.za>
2017-09-17 22:29:15 +0200

    Fix loading animation

--- public/function.js
+++ public/function.js
@@ -1,6 +1,4 @@
-document.querySelector("#loader").addEventListener("click", function() {
-  console.log("Done offload!");
-  setTimeout(function() {
+setTimeout(function() {
     document.querySelector("#loader").style.display = "none";
     document.querySelector("#feedback").style.display = "block";
-  }, 1000)});
+}, 1000);
